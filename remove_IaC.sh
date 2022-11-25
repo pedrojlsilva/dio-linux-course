140c6d29d954a4df7ce7bf47da833fa0330e1004
Pedro Jorge Silva <pjls2@cin.ufpe.br>
2022-11-25 19:36:24 -0300

    add script to remove IaC created by script

---
+++ remove_IaC.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+rm -Rf /publico
+rm -Rf /adm
+rm -Rf /ven
+rm -Rf /sec
+
+userdel -r carlos 
+userdel -r maria 
+userdel -r joao 
+userdel -r debora 
+userdel -r sebastiana 
+userdel -r roberto 
+userdel -r josefina 
+userdel -r amanda 
+userdel -r rogerio 
+
+
+groupdel GRP_ADM
+groupdel GRP_VEN
+groupdel GRP_SEC
