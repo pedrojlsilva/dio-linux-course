0e6dc00aa24bb4418f0a6b28cecbafac133cd3a9
Pedro Jorge Silva <pjls2@cin.ufpe.br>
2022-11-26 12:02:03 -0300

    fix name of folder

--- apacheServer.sh
+++ apacheServer.sh
@@ -15,7 +15,7 @@ cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 
 unzip main.zip
-cd linux-site-dio
+cd linux-site-dio-main
 cp -R * /var/www/html
 
 echo "fim"
